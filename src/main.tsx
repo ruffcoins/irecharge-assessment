a6bdf0242424282785e922a173f8bd8035263d80
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2024-10-02 11:21:19 +0100

    feat: homepage

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CityDetail from './components/CityDetail.tsx';
+import HomeLayout from './components/layout/HomeLayout.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout><App /></HomeLayout>,
+  },
+  {
+    path: "/city-detail/:city",
+    element: <HomeLayout><CityDetail /></HomeLayout>
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
